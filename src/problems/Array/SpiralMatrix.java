28b09ba9371ddf12fe9e8957ffa377aa72a415b7
ngnanavel <ngnanavel@ebay.com>
2021-02-15 19:20:57 -0800

    Spiral Matrix steps

--- src/problems/Array/SpiralMatrix.java
+++ src/problems/Array/SpiralMatrix.java
@@ -1,5 +1,13 @@
 package problems.Array;
 
+/**************
+ * Step1 : Initalize top,bottom,left & right..dir=1
+ * Step2 : print top row => from left->right => top++ & dir =2
+ * Step3: print right column => from top to bottom => right-- & dir =3
+ * Step4: print bottom row => from right to left => bottom-- & dir =4
+ * Step5: print left column => from bottom to top => left++ & dir =1
+ * Step6: repeat above steps until top=>bottom && left=>right is not satisfied
+ **************** */
 public class SpiralMatrix {
     public static void main(String args[]){
         int[][] array = {{1,2,3,4},{12,13,14,5},{11,16,15,6},{10,9,8,7}};
